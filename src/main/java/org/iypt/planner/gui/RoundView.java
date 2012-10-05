64e5429437d190493b09953fe0f0dde1c13272a1
Jiří Locker <jiri.locker@gmail.com>
2012-10-05 23:31:27 +0200

    Change RoundView to Expander
    Visually more distinct.

--- src/main/java/org/iypt/planner/gui/RoundView.java
+++ src/main/java/org/iypt/planner/gui/RoundView.java
@@ -2,9 +2,8 @@ package org.iypt.planner.gui;
 
 import org.apache.pivot.wtk.BoxPane;
 import org.apache.pivot.wtk.Container;
-import org.apache.pivot.wtk.Label;
+import org.apache.pivot.wtk.Expander;
 import org.apache.pivot.wtk.Orientation;
-import org.apache.pivot.wtk.Rollup;
 import org.iypt.planner.domain.Group;
 import org.iypt.planner.domain.Round;
 import org.iypt.planner.domain.Tournament;
@@ -13,21 +12,18 @@ import org.iypt.planner.domain.Tournament;
  *
  * @author jlocker
  */
-public class RoundView extends Rollup {
+public class RoundView extends Expander {
 
-    private Label roundLabel = new Label();
     private Container content = new BoxPane(Orientation.VERTICAL);
 
     public RoundView() {
-        super(true);
-        setHeading(roundLabel);
+        super();
+        super.setExpanded(true);
         setContent(content);
     }
 
     public void update(Tournament tournament, Round round) {
-        roundLabel.setText("Round #" + round.getNumber());
-        roundLabel.setTooltipText("Day " + round.getDay());
-        roundLabel.setTooltipDelay(300);
+        this.setTitle("Round #" + round.getNumber());
 
 //        content.clear();
         for (Group group : round.getGroups()) {
