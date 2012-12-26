148b84269a32c497137ee09cf0e90e42a41d7202
Jiří Locker <jiri.locker@gmail.com>
2012-12-26 21:42:06 +0100

    Adding ScheduleWriter and it's test

---
+++ src/main/java/org/iypt/planner/csv/ScheduleWriter.java
@@ -0,0 +1,52 @@
+package org.iypt.planner.csv;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+import org.iypt.planner.domain.Group;
+import org.iypt.planner.domain.Juror;
+import org.iypt.planner.domain.JurySeat;
+import org.iypt.planner.domain.Round;
+import org.iypt.planner.domain.Tournament;
+import org.supercsv.io.CsvListWriter;
+import org.supercsv.io.ICsvListWriter;
+import org.supercsv.prefs.CsvPreference;
+
+/**
+ *
+ * @author jlocker
+ */
+public class ScheduleWriter {
+
+    public ScheduleWriter(Tournament tournament) {
+        this.tournament = tournament;
+    }
+
+    private final Tournament tournament;
+
+    public void write(Writer writer) throws IOException {
+        try (ICsvListWriter listWriter = new CsvListWriter(writer, CsvPreference.EXCEL_NORTH_EUROPE_PREFERENCE)) {
+
+            // for each group
+            for (Round round : tournament.getRounds()) {
+                for (Group group : round.getGroups()) {
+                    List<Object> data = new ArrayList<>(tournament.getJuryCapacity() + 2);
+                    // append round number and group name
+                    data.add(round.getNumber());
+                    data.add("Group " + group.getName());
+                    // append all jurors
+                    for (JurySeat seat : group.getJury().getSeats()) {
+                        Juror juror = seat.getJuror();
+                        if (juror == null) {
+                            juror = Juror.NULL;
+                        }
+                        data.add(String.format("%s, %s", juror.getLastName(), juror.getFirstName()));
+                    }
+                    // write the line
+                    listWriter.write(data);
+                }
+            }
+        }
+    }
+}
---
+++ src/test/java/org/iypt/planner/csv/ScheduleWriterTest.java
@@ -0,0 +1,122 @@
+package org.iypt.planner.csv;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.iypt.planner.domain.Group;
+import org.iypt.planner.domain.Juror;
+import org.iypt.planner.domain.JurySeat;
+import org.iypt.planner.domain.Round;
+import org.iypt.planner.domain.Tournament;
+import org.iypt.planner.domain.util.CSVTournamentFactory;
+import org.iypt.planner.domain.util.RoundFactory;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.iypt.planner.domain.util.SampleFacts.*;
+import static org.junit.Assert.assertThat;
+
+/**
+ *
+ * @author jlocker
+ */
+public class ScheduleWriterTest {
+
+    @BeforeClass
+    public static void setUp() throws IOException {
+        Round r1 = RoundFactory.createRound(1, gABC, gDEF);
+        Round r2 = RoundFactory.createRound(2, gABC, gDEF);
+        tournament = new Tournament();
+        tournament.setJuryCapacity(3);
+        tournament.addRounds(r1, r2);
+        List<Juror> jurors = Arrays.asList(jA1, jA2, jB2, jB2, jC1, jC2, jD1, jD2);
+        tournament.addJurors(jurors);
+        int i = 0;
+        for (Round round : tournament.getRounds()) {
+            for (Group group : round.getGroups()) {
+                for (JurySeat seat : group.getJury().getSeats()) {
+                    seat.setJuror(jurors.get(i));
+                    i = (++i) % jurors.size();
+                }
+            }
+        }
+    }
+    private static final Logger log = LoggerFactory.getLogger(ScheduleWriterTest.class);
+    private static Tournament tournament;
+
+    @Test
+    public void testDummyTournament() throws IOException {
+        StringWriter sw = new StringWriter();
+        ScheduleWriter writer = new ScheduleWriter(tournament);
+        writer.write(sw);
+
+        log.debug("\n[{}]", sw.toString());
+
+        List<String> actualLines = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new StringReader(sw.toString()));
+
+        // read all lines
+        int lines = tournament.getGroups().size();
+        for (int i = 0; i < lines; i++) {
+            actualLines.add(br.readLine());
+        }
+
+        // all lines have been read
+        assertThat(br.readLine(), nullValue());
+
+        // check the stored lines
+        String[] expectedLines = {
+            "1;Group A;1, null;2, null;1, null",
+            "1;Group B;2, null;2, null;2, null",
+            "2;Group A;1, null;2, null;1, null",
+            "2;Group B;2, null;2, null;2, null"};
+        assertThat(actualLines, contains(expectedLines));
+    }
+
+    @Test
+    public void testInputOutpuMatch() throws IOException {
+        // parse IYPT 2012 schedule
+        CSVTournamentFactory factory = new CSVTournamentFactory();
+        factory.readDataFromClasspath("/org/iypt/planner/csv/", "team_data.csv", "jury_data.csv", "schedule2012.csv");
+        Tournament t = factory.newTournament();
+
+        // write it
+        StringWriter sw = new StringWriter();
+        ScheduleWriter writer = new ScheduleWriter(t);
+        writer.write(sw);
+
+        // and read the first line written
+        String actual = new BufferedReader(new StringReader(sw.toString())).readLine();
+
+        // read the source file
+        InputStreamReader isr = new InputStreamReader(ScheduleWriterTest.class.getResourceAsStream("/org/iypt/planner/csv/schedule2012.csv"));
+        String expected = new BufferedReader(isr).readLine();
+
+        // verify that what has been written is exactly what has been read (first line only)
+        log.info("Comparing input/output:\n[{}]\n[{}]", expected, actual);
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void testNullJuror() throws IOException {
+        Tournament t = new Tournament();
+        t.setJuryCapacity(2);
+        t.addRounds(RoundFactory.createRound(1, gABC));
+        // writer should not fail to write null jurors
+        StringWriter sw = new StringWriter();
+        ScheduleWriter writer = new ScheduleWriter(t);
+        writer.write(sw);
+        log.debug("[{}]", sw.toString());
+        assertThat(sw.toString(), is("1;Group A;null, null;null, null\n"));
+    }
+}
