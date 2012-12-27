d28709cdf82614ffd12e9d1435869f90464a5aba
Jiří Locker <jiri.locker@gmail.com>
2012-12-27 20:33:44 +0100

    Adding BiasReader plus test and bias data

---
+++ src/main/java/org/iypt/planner/csv/BiasReader.java
@@ -0,0 +1,78 @@
+package org.iypt.planner.csv;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import org.supercsv.cellprocessor.ParseDouble;
+import org.supercsv.cellprocessor.constraint.NotNull;
+import org.supercsv.cellprocessor.ift.CellProcessor;
+import org.supercsv.io.CsvBeanReader;
+import org.supercsv.io.ICsvBeanReader;
+import org.supercsv.prefs.CsvPreference;
+
+/**
+ *
+ * @author jlocker
+ */
+public class BiasReader {
+
+    public static class BiasRow {
+
+        private String given_name;
+        private String last_name;
+        private double bias;
+
+        public void setBias(double bias) {
+            this.bias = bias;
+        }
+
+        public void setLast_name(String last_name) {
+            this.last_name = last_name;
+        }
+
+        public void setGiven_name(String given_name) {
+            this.given_name = given_name;
+        }
+
+        public String getFullName() {
+            return String.format("%s %s", given_name, last_name);
+        }
+
+        public double getBias() {
+            return bias;
+        }
+    }
+    private static final CellProcessor[] processors = new CellProcessor[]{
+        new NotNull(),
+        new NotNull(),
+        new ParseDouble()
+    };
+    private final Map<String, Double> rows = new HashMap<>(100);
+
+    public Map<String, Double> read(Reader reader) throws IOException {
+        if (!rows.isEmpty()) {
+            throw new UnsupportedOperationException("Repeated reading not supported.");
+        }
+        try (ICsvBeanReader beanReader = new CsvBeanReader(reader, CsvPreference.STANDARD_PREFERENCE)) {
+
+            // the header elements are used to map the values to the bean (names must match)
+            final String[] header = beanReader.getHeader(true);
+            BiasRow bean;
+            while ((bean = beanReader.read(BiasRow.class, header, processors)) != null) {
+                rows.put(bean.getFullName(), bean.getBias());
+            }
+
+        }
+        return getRows();
+    }
+
+    public Map<String, Double> getRows() {
+        return Collections.unmodifiableMap(rows);
+    }
+
+    public double getBias(String jurorName) {
+        return rows.get(jurorName);
+    }
+}
---
+++ src/test/java/org/iypt/planner/csv/BiasReaderTest.java
@@ -0,0 +1,28 @@
+package org.iypt.planner.csv;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.closeTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ *
+ * @author jlocker
+ */
+public class BiasReaderTest {
+
+    private static final double ERROR = Double.MIN_VALUE;
+
+    @Test
+    public void testData() throws IOException {
+        BiasReader reader = new BiasReader();
+        reader.read(new InputStreamReader(BiasReaderTest.class.getResourceAsStream("bias_IYPT2012.csv")));
+        assertThat(reader.getRows().size(), is(82));
+        assertThat(reader.getBias("Martin Plesch"), closeTo(-0.7, ERROR));
+        assertThat(reader.getBias("Joy Tan"), closeTo(0d, ERROR));
+        assertThat(reader.getBias("Michael Gierling"), closeTo(0.89, ERROR));
+    }
+}
