088c7a5e3201c8dd0c318c4885fc3c698b016e44
Tomáš Locker <tomas.locker@gmail.com>
2013-05-29 21:12:27 +0200

    pdf test

--- src/test/java/org/iypt/planner/pdf/PdfTest.java
+++ src/test/java/org/iypt/planner/pdf/PdfTest.java
@@ -3,6 +3,7 @@ package org.iypt.planner.pdf;
 import com.itextpdf.text.DocumentException;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.Iterator;
 import org.iypt.planner.csv.CSVTournamentFactory;
 import org.iypt.planner.domain.Juror;
@@ -32,31 +33,11 @@ public class PdfTest {
     }
     
     @Test
-    public void test() throws DocumentException, FileNotFoundException{
-        /*Tournament t = new Tournament();
-        Round r1 = RoundFactory.createRound(1, tA, tB, tC, tD, tE, tF, tG, tH, tI);
-        Round r2 = RoundFactory.createRound(2, tA, tB, tC, tD, tE, tF, tG, tH, tI);
-        Round r3 = RoundFactory.createRound(3, tA, tB, tC, tD, tE, tF, tG, tH, tI);
-        Round r4 = RoundFactory.createRound(4, tA, tB, tC, tD, tE, tF, tG, tH, tI);
-
-        t.addRounds(r1);
-        t.addRounds(r2);
-        t.addRounds(r3);
-        t.addRounds(r4);
-        t.setJuryCapacity(1);
-        assignJurors(t, jM1, jN2, jN1, jM2);*/
-        
-        
+    public void test() throws DocumentException, FileNotFoundException, SQLException, IOException{
         
         PdfCreator c = new PdfCreator();
-        c.createPdf(tournament);
-    }
-
-
-    private void assignJurors(Tournament t, Juror... jurors) {
-        Iterator<Seat> it = t.getSeats().iterator();
-        for (int i = 0; i < jurors.length; i++) {
-            it.next().setJuror(jurors[i]);
-        }
+        
+        c.classPdf(tournament);
+        c.roundPdf(tournament);
     }
 }
