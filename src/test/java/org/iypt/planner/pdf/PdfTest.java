6aa73338edba8b897f9510b11b7f02f6ff2e302f
Jiří Locker <jiri.locker@gmail.com>
2013-07-12 21:43:23 +0200

    Code style

--- src/test/java/org/iypt/planner/pdf/PdfTest.java
+++ src/test/java/org/iypt/planner/pdf/PdfTest.java
@@ -4,26 +4,19 @@ import com.itextpdf.text.DocumentException;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.Iterator;
 import org.iypt.planner.csv.CSVTournamentFactory;
-import org.iypt.planner.domain.Juror;
-import org.iypt.planner.domain.Round;
-import org.iypt.planner.domain.Seat;
 import org.iypt.planner.domain.Tournament;
-import org.iypt.planner.domain.util.RoundFactory;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
-import static org.iypt.planner.domain.util.SampleFacts.*;
 
 /**
  *
  * @author Falen
  */
 public class PdfTest {
-    
+
     private static Tournament tournament;
-    
+
     @BeforeClass
     public static void setupTournament() throws IOException {
         CSVTournamentFactory factory = new CSVTournamentFactory();
@@ -31,12 +24,11 @@ public class PdfTest {
         tournament = factory.newTournament();
         tournament.setJuryCapacity(6);
     }
-    
+
     @Test
-    public void test() throws DocumentException, FileNotFoundException, SQLException, IOException{
-        
+    public void test() throws DocumentException, FileNotFoundException, SQLException, IOException {
         PdfCreator c = new PdfCreator();
-        
+
         c.classPdf(tournament);
         c.roundPdf(tournament);
     }
